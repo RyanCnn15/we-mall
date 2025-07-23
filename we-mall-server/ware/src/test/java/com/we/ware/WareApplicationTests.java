3bc120b905de8c3617582e7999139098871d763b
chenji <chenji@spaceicloud.com>
2025-07-23 22:51:07 +0800

    初始化项目

---
+++ we-mall-server/ware/src/test/java/com/we/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.we.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
